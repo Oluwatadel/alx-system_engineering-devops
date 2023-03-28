8d36a06c267bf8995ac8f91e643671ebb912ccfa
Oluwatobi Adelesi <oluwatobiadelesi@gmail.com>
2023-03-28 22:17:12 +0100

    all About ngnix

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,21 @@
+# Puppet Manifest to setup a nginx server
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'server_config':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Holberton School',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
